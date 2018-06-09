7fb65251ca5c9029edcce42d4b52d8bec7360447
Dmytro Yurchenko <dmytro.yurchenko@ti8m.ch>
2018-06-09 19:22:07 +0200

    Straightforward solution for code interview :)

---
+++ src/main/java/app/DatabaseService.java
@@ -0,0 +1,144 @@
+package app;
+
+import model.Database;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+public class DatabaseService implements Database {
+
+    static final String DB_FOLDER = "db";
+    private static final String FILE_EXTENSION = ".csv";
+    private static final String SEPARATOR = ";";
+
+    private Map<String, AtomicInteger> tableSeqMap = new HashMap<>();
+    private Path dbFolder;
+    private boolean initialized = false;
+
+    public DatabaseService() {
+        dbFolder = Paths.get(DB_FOLDER);
+
+    }
+
+
+    @Override
+    public int insert(String tableName, List<String> values) {
+        if (!initialized) {
+            this.initDb();
+        }
+
+        if (!tableSeqMap.containsKey(tableName)) {
+            tableSeqMap.put(tableName, new AtomicInteger(-1));
+        }
+
+        AtomicInteger tableLock = tableSeqMap.get(tableName);
+        int id = tableLock.incrementAndGet();
+        final Path path = dbFolder.resolve(tableName + FILE_EXTENSION);
+        try {
+            synchronized (tableLock) {
+                Files.write(path,
+                        Collections.singletonList(createEntry(id, values)),
+                        StandardCharsets.UTF_8,
+                        Files.exists(path) ? StandardOpenOption.APPEND : StandardOpenOption.CREATE);
+            }
+        } catch (final IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        return id;
+    }
+
+    @Override
+    public boolean update(String tableName, List<String> values, int id) {
+        if (!initialized) {
+            this.initDb();
+        }
+
+        AtomicInteger tableLock = tableSeqMap.get(tableName);
+        final Path path = dbFolder.resolve(tableName + FILE_EXTENSION);
+        String idMask = Integer.toString(id) + SEPARATOR;
+        boolean updated = false;
+        try {
+            List<String> entries = Files.readAllLines(path, StandardCharsets.UTF_8);
+            synchronized (tableLock) {
+                for (int i = 0; i < entries.size(); i++) {
+                    String line = entries.get(i);
+                    if (line.startsWith(idMask)) {
+                        entries.remove(line);
+                        entries.add(createEntry(id, values));
+                        updated = true;
+                    }
+                }
+                if (updated) {
+                    Files.write(path, entries, StandardCharsets.UTF_8,
+                            StandardOpenOption.WRITE,
+                            StandardOpenOption.TRUNCATE_EXISTING);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return updated;
+    }
+
+    @Override
+    public List<String> select(String tableName, int id) {
+        String idMask = Integer.toString(id) + SEPARATOR;
+        try {
+            Scanner fileScanner = new Scanner(dbFolder.resolve(tableName + FILE_EXTENSION));
+            while (fileScanner.hasNextLine()) {
+                String line = fileScanner.nextLine();
+                if (line.startsWith(idMask)) {
+                    return Arrays.asList(line.split(SEPARATOR));
+                }
+            }
+        } catch (IOException e) {
+            return null;
+        }
+
+        return null;
+    }
+
+    private static String createEntry(int id, List<String> values) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(id).append(SEPARATOR);
+        values.forEach(value -> sb.append(value).append(SEPARATOR));
+        return sb.toString();
+    }
+
+    private void initDb(){
+
+        if (!Files.exists(dbFolder)) {
+            try {
+                Files.createDirectory(dbFolder);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            try {
+                Files.list(dbFolder)
+                        .forEach(file -> {
+                            String tableName = file.getFileName().toString().replace(FILE_EXTENSION, "");
+                            try {
+                                List<String> entries = Files.readAllLines(file, StandardCharsets.UTF_8);
+                                int maxId = entries.stream().map(e -> Integer.parseInt(e.split(SEPARATOR)[0]))
+                                        .collect(Collectors.summarizingInt(Integer::intValue)).getMax();
+                                tableSeqMap.put(tableName, new AtomicInteger(maxId));
+                            } catch (Exception e) {
+                                throw new RuntimeException();
+                            }
+                        });
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
---
+++ src/test/java/app/DatabaseControllerTests.java
@@ -0,0 +1,109 @@
+package app;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.io.File;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class DatabaseControllerTests {
+
+    public static final MediaType APPLICATION_JSON_UTF8 = new MediaType(MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
+
+
+    @Autowired
+    private MockMvc mockMvc;
+    private ObjectWriter ow;
+
+    @Before
+    public void init() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+        ow = mapper.writer().withDefaultPrettyPrinter();
+        File dbFolder = new File(DatabaseService.DB_FOLDER);
+
+        // deleting all test files for re-runnability
+        File[] dbFiles = dbFolder.listFiles();
+        if (dbFiles != null) {
+            for (File file :dbFiles) {
+                if (file.getName().startsWith("test")) {
+                    file.delete();
+                }
+            }
+        }
+    }
+
+    @Test
+    public void testSimpleCRUD() throws Exception {
+        int i = 1;
+        String result = this.mockMvc.perform(post("/insert/testTable")
+                .contentType(APPLICATION_JSON_UTF8)
+                .content(ow.writeValueAsString(Arrays.asList("value" + i++, "value" + i++))))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+
+        assertEquals("0", result);
+
+        result = this.mockMvc.perform(post("/insert/testTable")
+                .contentType(APPLICATION_JSON_UTF8)
+                .content(ow.writeValueAsString(Arrays.asList("value" + i++, "value" + i++))))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+
+        assertEquals("1", result);
+
+        result = this.mockMvc.perform(get("/select/testTable/1")
+                .contentType(APPLICATION_JSON_UTF8))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+        assertEquals("[\"1\",\"value3\",\"value4\"]", result);
+
+        result = this.mockMvc.perform(get("/select/testTable/0")
+                .contentType(APPLICATION_JSON_UTF8))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+        assertEquals("[\"0\",\"value1\",\"value2\"]", result);
+
+        result = this.mockMvc.perform(post("/update/testTable/1")
+                .contentType(APPLICATION_JSON_UTF8)
+                .content(ow.writeValueAsString(Arrays.asList("value10", "value11"))))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+
+        assertEquals("true", result);
+
+        result = this.mockMvc.perform(get("/select/testTable/1")
+                .contentType(APPLICATION_JSON_UTF8))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+        assertEquals("[\"1\",\"value10\",\"value11\"]", result);
+    }
+}
